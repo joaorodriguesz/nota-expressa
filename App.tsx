f2f88b871d7958bc0a5c59daa3cd58c1decd01aa
joao.rodrigues <joaopaulomoreirarodrigues@gmail.com>
2023-09-25 17:29:53 -0300

    starter

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/Main/app';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aaa</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/components/Main/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Title: React.FC = () => {
+    return (
+      <View>
+        <Text>TEXT2</Text>
+      </View>
+    );
+  };
+
+export default Title;
